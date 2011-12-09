02696112c9a54f62bc866ba9367d2827f3afc4f7
Brian McSweeney <btmcsweeney@gmail.com>
2011-12-09 22:35:57 +0000

    Item searching now uses Mongo database rather than old MySQL

--- htdocs/ajax/searchcount.ajax.php
+++ htdocs/ajax/searchcount.ajax.php
@@ -2,6 +2,7 @@
 require_once "../inc/functions.inc.php";
 if (isset($_POST['id'])){
     $id=clean_int($_POST['id']);
-    mysql_query("update rsge.items set searches=(searches+1) where id=$id");
+    //mysql_query("update rsge.items set searches=(searches+1) where id=$id");
+    $itemsColl->update(array("id"=>$id), array('$inc'=> array('searches'=>1)));
 }
 ?>

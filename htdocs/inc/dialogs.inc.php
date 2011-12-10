a555b5fa20abbc828dee2b1597f2644c9dd194de
Brian McSweeney <btmcsweeney@gmail.com>
2011-12-10 01:46:59 +0000

    Cleaning up

--- htdocs/inc/dialogs.inc.php
+++ htdocs/inc/dialogs.inc.php
@@ -1,23 +1,4 @@
 <?php require_once "inc/functions.inc.php"; ?>
 <div class="hidden">
-    <div id="saveAsDialog" title="Save Graph As...">Title:<br><input type="text" name="title" id="saveGraphTitle"></div>
-    <div id="newCalcDialog" title="Select New Calculation Type">
-	<button value="merchanting">Merchanting</button>
-	<button value="custom">Custom</button>
-    </div>
-    <div id="calcSyntaxDialog" title="Calculation Syntax">
-	The base code for calculations works just like it would if you typed it into a calculator, with the exception of a few special operators that allow you to insert dynamic values. These are:
-	<ul>
-	    <li>
-		Item prices: Putting the ID of an item inside brackets [] will return the current price of that item. You can look up item IDs using the ID lookup search bar.<br>
-		ex: The code [2]+100 would result in 246+100 = 346.
-	    <li>
-		Input fields: Putting curly braces {} into a calculation will result in an input field being generated in the resulting calculation, allowing you to change that value without editing the calculation's source code. You can set a default value by putting that value inside the braces.<br>
-		ex: The code {5}*4 would result in <input type="text" value="5" size="5">*4 = 20.
-	    </li>
-	    <li>
-		EXP lookup: Putting a username in quotes will return the total EXP of that user. If the username is followed by a colon and a skill, that user's EXP for that skill will be returned.<br>
-		ex: The code 200000000-"Zezima:Dungeoneering" would result in 200,000,000-140,017,408 = 59,982,592.
-	    </li>
-    </div>
+
 </div>
--- htdocs/js/script.js
+++ htdocs/js/script.js
@@ -1,321 +1,227 @@
-var graph, curGraph=0, graphName, mode='main', savedGraphs;
-var items=[];
+var graph, curGraph = 0,
+    graphName, mode = 'main',
+    savedGraphs;
+var items = [];
 
-function makeGraph(title){
-    graph=new Highcharts.Chart({
-	chart: {
-	    renderTo: 'graph',
-	    type: 'spline',
-	    reflow: false,
-	    height: $(window).height()-$('header').outerHeight()-$('footer').outerHeight()-$('#top-ad').outerHeight()-$('nav').outerHeight()-$('.inner-header').outerHeight()
-	},
-	title: {
-	    text: title
-	},
-	xAxis: {
-	    type: 'datetime',
-	    dateTimeLabelFormats: {
-		month: '%e. %b',
-		year: '%b'
-	    },
-	    maxPadding: 0.03
-	},
-	yAxis: {
-	    title: 'Price'
-	},
-	tooltip: {
-	    formatter: function() {
-		return Highcharts.dateFormat('%A, %b. %e', this.x) +'<br/>'+
-		addCommas(this.y) +' gp';
-	    }
-	}
+function makeGraph(title) {
+    graph = new Highcharts.Chart({
+        chart: {
+            renderTo: 'graph',
+            type: 'spline',
+            reflow: false,
+            height: $(window).height() - $('header').outerHeight() - $('footer').outerHeight() - $('#top-ad').outerHeight() - $('nav').outerHeight() - $('.inner-header').outerHeight()
+        },
+        title: {
+            text: title
+        },
+        xAxis: {
+            type: 'datetime',
+            dateTimeLabelFormats: {
+                month: '%e. %b',
+                year: '%b'
+            },
+            maxPadding: 0.03
+        },
+        yAxis: {
+            title: 'Price'
+        },
+        tooltip: {
+            formatter: function() {
+                return Highcharts.dateFormat('%A, %b. %e', this.x) + '<br/>' + addCommas(this.y) + ' gp';
+            }
+        }
     });
     $(".highcharts-container text:contains('Highcharts')").remove();
 }
 
-function checkForUpdate()  {
+function checkForUpdate() {
     $.get('ajax/checkupdate.ajax.php', function(result) {
-	$('#update-times').html(result);
+        $('#update-times').html(result);
     });
     setTimeout("checkForUpdate();", 15000);
 }
 
-function addCommas(nStr)
-{
+function addCommas(nStr) {
     nStr += '';
     var x = nStr.split('.');
     var x1 = x[0];
     var x2 = x.length > 1 ? '.' + x[1] : '';
     var rgx = /(\d+)(\d{3})/;
     while (rgx.test(x1)) {
-	x1 = x1.replace(rgx, '$1' + ',' + '$2');
+        x1 = x1.replace(rgx, '$1' + ',' + '$2');
     }
     return x1 + x2;
 }
 
 function setHeight() {
-    var mainHeight=$(window).height()-$('header').outerHeight()-$('footer').outerHeight()-$('#top-ad').outerHeight()-$('nav').outerHeight();
+    var mainHeight = $(window).height() - $('header').outerHeight() - $('footer').outerHeight() - $('#top-ad').outerHeight() - $('nav').outerHeight();
     $('#main-container').height(mainHeight);
-    $('aside').height($(window).height()-$('header').outerHeight()-$('nav').outerHeight());
-    graph.setSize($(window).width()-144, mainHeight-$('.inner-header').outerHeight());
+    $('aside').height($(window).height() - $('header').outerHeight() - $('nav').outerHeight());
+    graph.setSize($(window).width() - 144, mainHeight - $('.inner-header').outerHeight());
 }
 
-function addItem(id, minTime) {
-    curGraph=id;
+function graphItem(id, minTime) {
+    curGraph = id;
     while (graph.series.length) {
-	graph.series[0].remove(false);
+        graph.series[0].remove(false);
     }
-    $.post('ajax/getprices.ajax.php', {"id": id, "mintime": minTime}, function(result) {
-	if (result!="") {
-	    var series={data: []};
-	    var values=result.split(';');
-	    $.each(values, function(valNum, val){
-		if (valNum===0){
-		    series.name=val;
-		    graphName=val;
-		} else {
-		    var point=[];
-		    var points=val.split(',');
-		    $.each(points, function(pointNum, pointVal){
-			    point.push(pointVal*1);
-		    });
-		    series.data.push(point);
-		}
-	    });
-	    if (graph==null) { makeGraph(graphName); }
-	    graph.addSeries(series);
-	    $('#addItemInput').val('');
-	    if ($('.savedGraphButton[value='+id+']').length>0) {
-		$('#saveGraphButton').html('Unsave');
-	    } else {
-		$('#saveGraphButton').html('Save');
-	    }
-        $('#saveGraphButton').removeClass("hidden");
-        //$('#timeRangeSelect').removeClass("hidden");
-        $('#timeRangeSelect_chzn').removeClass("hidden");
-	}
+    $.post('ajax/getprices.ajax.php', {
+        "id": id,
+        "mintime": minTime
+    }, function(result) {
+        if (result != "") {
+            var series = {
+                data: []
+            };
+            var values = result.split(';');
+            $.each(values, function(valNum, val) {
+                if (valNum === 0) {
+                    series.name = val;
+                    graphName = val;
+                } else {
+                    var point = [];
+                    var points = val.split(',');
+                    $.each(points, function(pointNum, pointVal) {
+                        point.push(pointVal * 1);
+                    });
+                    series.data.push(point);
+                }
+            });
+            if (graph == null) {
+                makeGraph(graphName);
+            }
+            graph.addSeries(series);
+            $('#graphItemInput').val('');
+            if ($('.savedGraphButton[value=' + id + ']').length > 0) {
+                $('#saveGraphButton').html('Unsave');
+            } else {
+                $('#saveGraphButton').html('Save');
+            }
+            $('#saveGraphButton').removeClass("hidden");
+            //$('#timeRangeSelect').removeClass("hidden");
+            $('#timeRangeSelect_chzn').removeClass("hidden");
+        }
     });
     $('#graph').html('<img src="img/loader.gif">');
-    graph=null;
+    graph = null;
     $('#saveGraphButton').addClass("hidden");
     //$('#timeRangeSelect').addClass("hidden");
     $('#timeRangeSelect_chzn').addClass("hidden");
 }
 
 function getSavedGraphs() {
-    if ($.cookie('items')!=null) {
-	savedGraphs=$.cookie('items').split('-');
-	for (var saved in savedGraphs) {
-	    var i=savedGraphs[saved].split('_');
-	    $('#userGraphs').append('<button class="savedGraphButton" value="'+i[0]+'">'+i[1]+'</button>');
-	}
+    if ($.cookie('items') != null) {
+        savedGraphs = $.cookie('items').split('-');
+        for (var saved in savedGraphs) {
+            var i = savedGraphs[saved].split('_');
+            $('#userGraphs').append('<button class="savedGraphButton" value="' + i[0] + '">' + i[1] + '</button>');
+        }
     }
 }
 
 function saveGraph(id) {
-    if ($('#saveGraphButton').html()=='Unsave') {
-	savedGraphs=$.cookie('items').split('-');
-	$.cookie('items', null);
-	for (saved in savedGraphs) {
-	    i=savedGraphs[saved].split('_');
-	    if (i[0]!=id) {
-		if (saved!=0) {
-		    $.cookie('items', ($.cookie('items')+'-'+i[0]+'_'+i[1]), { expires: 500});
-		} else {
-		    $.cookie('items', i[0]+'_'+i[1], { expires: 500});
-		}
-	    }
-	}
-	$('.savedGraphButton[value='+id+']').hide();
+    if ($('#saveGraphButton').html() == 'Unsave') {
+        savedGraphs = $.cookie('items').split('-');
+        $.cookie('items', null);
+        for (var saved in savedGraphs) {
+            var i = savedGraphs[saved].split('_');
+            if (i[0] != id) {
+                if (saved != 0) {
+                    $.cookie('items', ($.cookie('items') + '-' + i[0] + '_' + i[1]), {
+                        expires: 500
+                    });
+                } else {
+                    $.cookie('items', i[0] + '_' + i[1], {
+                        expires: 500
+                    });
+                }
+            }
+        }
+        $('.savedGraphButton[value=' + id + ']').hide();
     } else {
-	if ($.cookie('items')!=null) {
-	    $.cookie('items', ($.cookie('items')+'-'+curGraph+'_'+graphName), { expires: 500});
-	} else {
-	    $.cookie('items', curGraph+'_'+graphName, { expires: 500});
-	}
-	$('#userGraphs').append('<button class="savedGraphButton" value="'+curGraph+'">'+graphName+'</button>');
-	$('.savedGraphButton').click(function(){
-	    addItem($(this).val(), $('#timeRangeSelect').val()*60*60*24);
-	});
-	$('#saveGraphButton').html('Unsave');
-    }
-}
-
-function unsaveGraph(id) {
-    savedGraphs=$.cookie('items').split('-');
-    for (var saved in savedGraphs) {
-	var i=savedGraphs[saved].split('_');
-	if (i[0]!=id) {
-	    if (saved!=0) {
-		$.cookie('items', ($.cookie('items')+'-'+curGraph+'_'+graphName), { expires: 500});
-	    } else {
-		$.cookie('items', curGraph+'_'+graphName, { expires: 500});
-	    }
-	}
+        if ($.cookie('items') != null) {
+            $.cookie('items', ($.cookie('items') + '-' + curGraph + '_' + graphName), {
+                expires: 500
+            });
+        } else {
+            $.cookie('items', curGraph + '_' + graphName, {
+                expires: 500
+            });
+        }
+        $('#userGraphs').append('<button class="savedGraphButton" value="' + curGraph + '">' + graphName + '</button>');
+        $('.savedGraphButton').click(function() {
+            graphItem($(this).val(), $('#timeRangeSelect').val() * 60 * 60 * 24);
+        });
+        $('#saveGraphButton').html('Unsave');
     }
 }
 
 function loadGraphMode() {
-    //$.post('inc/graph.inc.php', function(result){
-	//$('#main-container').html(result);
-	makeGraph(null);
-	$('.chzn-select').chosen();
-    //$('#timeRangeSelect_chzn').addClass("hidden");
-	$('#timeRangeSelect_chzn').addClass("hidden").css('width','88px').children('.chzn-drop').css('width','86px');
-	$('.itemSearch').autocomplete({
-	    source: 'ajax/itemsearch.ajax.php',
-	    minLength: 2,
-	    select: function(event,ui){
-		$.post('ajax/searchcount.ajax.php', {"id": ui.item.id});
-		addItem(ui.item.id, $('#timeRangeSelect').val()*60*60*24);
-		items.push(ui.item.id);
-	}
-	}).data( 'autocomplete' )._renderItem = function( ul, item ) {
-	    return $( '<li></li>' )
-	    .data( 'item.autocomplete', item )
-	    .append( '<a><span class="asPrice">' + item.label + '</span></a><hr>' )
-	    .appendTo( ul );
-	};
-    
-	$('#addItem').submit(function(event) {
-	    event.preventDefault();
-	});
-	
-	$('#removeItemSelect').change(function(event,ui) {
-	    var seriesNum=$('#removeItemSelect').val();
-	    items.splice(seriesNum,1);
-	    graph.series[seriesNum].remove();
-	    var newOptions='<option value=""></option>';
-	    for (x in graph.series){
-		newOptions+='<option value="'+x+'">'+graph.series[x].name+'</option>';
-	    }
-	    $('#removeItemSelect').empty().html(newOptions).trigger('liszt:updated');
-	});
-	
-	$('#timeRangeSelect').change(function(event,ui) {
-	    var seconds=$('#timeRangeSelect').val()*60*60*24;
-	    while (graph.series.length) {
-		graph.series[0].remove(false);
-	    }
-	    $('#removeItemSelect').empty().html('<option value=""></option>').trigger('liszt:updated');
-	    addItem(curGraph, seconds);
-	    graph.redraw();
-	});
-	
-	$('#saveGraphButton').click(function(){
-	    saveGraph(curGraph);
-	});
-	
-	$('#deleteGraphButton').click(function(){
-	    deleteGraph(curGraph);
-	});
-	mode='graph';
-    //});
-}
+    makeGraph(null);
+    $('.chzn-select').chosen();
+    $('#timeRangeSelect_chzn').addClass("hidden").css('width', '88px').children('.chzn-drop').css('width', '86px');
+    $('.itemSearch').autocomplete({
+        source: 'ajax/itemsearch.ajax.php',
+        minLength: 2,
+        select: function(event, ui) {
+            $.post('ajax/searchcount.ajax.php', {
+                "id": ui.item.id
+            });
+            graphItem(ui.item.id, $('#timeRangeSelect').val() * 60 * 60 * 24);
+            items.push(ui.item.id);
+        }
+    }).data('autocomplete')._renderItem = function(ul, item) {
+        return $('<li></li>').data('item.autocomplete', item).append('<a><span class="asPrice">' + item.label + '</span></a><hr>').appendTo(ul);
+    };
 
-function loadCalculatorMode() {
-    $.post('inc/calculator.inc.php', function(result){
-	$('#main-container').html(result);
+    $('#graphItem').submit(function(event) {
+        event.preventDefault();
     });
-    $('#newCalcButton').click(function(){
-	$('#newCalcDialog').dialog('open');
+
+    $('#removeItemSelect').change(function(event, ui) {
+        var seriesNum = $('#removeItemSelect').val();
+        items.splice(seriesNum, 1);
+        graph.series[seriesNum].remove();
+        var newOptions = '<option value=""></option>';
+        for (var x in graph.series) {
+            newOptions += '<option value="' + x + '">' + graph.series[x].name + '</option>';
+        }
+        $('#removeItemSelect').empty().html(newOptions).trigger('liszt:updated');
     });
-    
-    $('#switchMode').html('Switch to Graph');
-    mode='calculator';
+
+    $('#timeRangeSelect').change(function(event, ui) {
+        var seconds = $('#timeRangeSelect').val() * 60 * 60 * 24;
+        while (graph.series.length) {
+            graph.series[0].remove(false);
+        }
+        $('#removeItemSelect').empty().html('<option value=""></option>').trigger('liszt:updated');
+        graphItem(curGraph, seconds);
+        graph.redraw();
+    });
+
+    $('#saveGraphButton').click(function() {
+        saveGraph(curGraph);
+    });
+
+    $('#deleteGraphButton').click(function() {
+        deleteGraph(curGraph);
+    });
+    mode = 'graph';
 }
 
 
 //Call on load
-$(function(){
+$(function() {
     loadGraphMode();
     setHeight();
-    //$.cookie('items', null);
     getSavedGraphs();
     checkForUpdate();
-    
-    $('#signUpButton').click(function(){
-	$('#signUpForm').dialog('open');
-    });
-    $('#signUpForm').dialog({
-	autoOpen: false,
-	width: 250,
-	resizable: false,
-	modal: true,
-	buttons: {
-	    'Create Account': function() {
-		    $(this).submit();
-		},
-	    'Cancel': function() {
-		$(this).dialog('close');
-	    }
-	},
-	close: function() {
-	    $('#signUpForm input').val('');
-	    $('#signUpStatus').html('');
-	}
-    })
-    .submit(function(event) {
-	event.preventDefault();
-	$('.ui-dialog-buttonset button').attr('disabled','disabled');
-	$('#signUpStatus').html("Please wait, processing...");
-	$.post('ajax/signup.ajax.php', $('#signUpForm').serialize(), function(result) {
-	    if (result=='ok') {
-		$('.ui-dialog-buttonpane').detach();
-		$('#signUpStatus').html("You must activate your account before you can log in. An activation email has been sent to the supplied address.");
-	    } else {
-		$('#signUpStatus').html(result);
-		$('.ui-dialog-buttonset button').removeAttr('disabled');
-	    }
-	});
+
+    $('.savedGraphButton').click(function() {
+        graphItem($(this).val(), $('#timeRangeSelect').val() * 60 * 60 * 24);
     });
     
-    $('#loginForm').submit(function(event) {
-	event.preventDefault();
-	$.post('ajax/login.ajax.php', $('#loginForm').serialize(), function(result) {
-	    if (result=='ok') {
-		location.reload();
-	    } else {
-		$('#loginError').html(result);
-	    }
-	});
+    $(window).resize(function() {
+        setHeight();
     });
-    
-    $('.savedGraphButton').click(function(){
-	addItem($(this).val(), $('#timeRangeSelect').val()*60*60*24);
-    });
-    $('.savedCalcSetButton').click(function(){
-	loadCalcSet($(this).val());
-    });
-    
-    $('#saveAsDialog').dialog({
-	autoOpen: false,
-	resizable: false,
-	modal: true,
-	buttons: {
-	    'Save Graph': function() {
-		saveGraphAs($('#saveGraphTitle').val());
-		$(this).dialog('close');
-	    }
-	}
-    });
-    
-    $('#newCalcDialog').dialog({
-	autoOpen: false,
-	modal: true,
-	width: 300
-    });
-    $('#newCalcDialog button').click(function(){
-	$('#newCalcDialog').dialog({title: 'New '+$(this).html()+' Calculator'});
-	$.post('ajax/calctypes.ajax.php', {'type': $(this).val()}, function(result){
-	    $('#newCalcDialog').html(result);
-	});
-    });
-    
-    $('#calcSyntaxDialog').dialog({
-	autoOpen: false,
-	width: 800
-    });
-    
-    $(window).resize(function(){setHeight();});
 });

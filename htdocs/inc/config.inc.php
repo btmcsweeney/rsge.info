9b19705cfd59d3bf53a9d33df99446fd43f5eafa
Brian McSweeney <btmcsweeney@gmail.com>
2011-12-10 00:58:38 +0000

    No more MySQL!

--- htdocs/inc/config.inc.php
+++ htdocs/inc/config.inc.php
@@ -1,6 +1,3 @@
 <?php
-$mysql_host="";
-$mysql_user="";
-$mysql_pass="";
 $analytics_id="UAXXXXXXXX1";
 ?>
--- htdocs/inc/dbconnect.inc.php
+++ htdocs/inc/dbconnect.inc.php
@@ -1,10 +1,5 @@
 <?php
 require_once "config.inc.php";
-$con = mysql_pconnect($mysql_host,$mysql_user,$mysql_pass);
-if (!$con)
-{
-  die('Could not connect: ' . mysql_error());
-}
 $mongo=new Mongo();
 $mdb=$mongo->rsge;
 $itemsColl=$mdb->items;

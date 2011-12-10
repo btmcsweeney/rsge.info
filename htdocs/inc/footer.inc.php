0981121ee7b60d690fae9505e0010ae725477818
Brian McSweeney <btmcsweeney@gmail.com>
2011-12-10 02:07:41 +0000

    Updated footer

--- htdocs/inc/footer.inc.php
+++ htdocs/inc/footer.inc.php
@@ -1,10 +1,8 @@
 <?php require_once "inc/functions.inc.php"; ?>
 <a href="http://www.w3.org/html/logo/">
     <img src="http://www.w3.org/html/logo/badge/html5-badge-h-css3-performance-semantics.png" width="74" height="24" alt="HTML5 Powered with CSS3 / Styling, Performance &amp; Integration, and Semantics" title="HTML5 Powered with CSS3 / Styling, Performance &amp; Integration, and Semantics">
-</a>
-<a href="http://www.mongodb.org">
+</a><a href="http://www.mongodb.org">
     <img src="http://www.mongodb.org/download/attachments/132305/logo-mongodb-ondark.png" height="24" title="Powered by MongoDB">
-</a>
-<a href="https://github.com/btmcsweeney/rsge.info">
+</a><a href="https://github.com/btmcsweeney/rsge.info">
     <img src="http://a248.e.akamai.net/assets.github.com/images/modules/header/logov6.svg" height="24" title="Open source project available on GitHub">
 </a>

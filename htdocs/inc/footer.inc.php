85bfbd9f752dc38b974d33f766833a6eb6f0f4d6
Brian McSweeney <btmcsweeney@gmail.com>
2011-12-10 02:29:59 +0000

    Footer links open in new tab

--- htdocs/inc/footer.inc.php
+++ htdocs/inc/footer.inc.php
@@ -1,4 +1,4 @@
 <?php require_once "inc/functions.inc.php"; ?>
-<a href="http://www.w3.org/html/logo/"><img src="img/html5.png" width="74" height="24" alt="HTML5 Powered with CSS3 / Styling, Performance &amp; Integration, and Semantics" title="HTML5 Powered with CSS3 / Styling, Performance &amp; Integration, and Semantics"></a>
-<a href="http://www.mongodb.org"><img src="img/mongodb.png" height="24" alt="Powered by MongoDB" title="Powered by MongoDB"></a>
-<a href="https://github.com/btmcsweeney/rsge.info"><img src="img/github.png" height="24" alt="Open source project available on GitHub" title="Open source project available on GitHub"></a>
+<a href="http://www.w3.org/html/logo/"><img src="img/html5.png" width="74" height="24" alt="HTML5 Powered with CSS3 / Styling, Performance &amp; Integration, and Semantics" title="HTML5 Powered with CSS3 / Styling, Performance &amp; Integration, and Semantics" target="_blank"></a>
+<a href="http://www.mongodb.org"><img src="img/mongodb.png" height="24" alt="Powered by MongoDB" title="Powered by MongoDB" target="_blank"></a>
+<a href="https://github.com/btmcsweeney/rsge.info"><img src="img/github.png" height="24" alt="Open source project available on GitHub" title="Open source project available on GitHub" target="_blank"></a>

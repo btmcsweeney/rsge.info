95e804b833eaebef755f65db207eaf923d2b3e85
Brian McSweeney <btmcsweeney@gmail.com>
2012-02-22 17:28:40 +0000

    "Got rid of the time range select in the top bar"

--- htdocs/inc/graph.inc.php
+++ htdocs/inc/graph.inc.php
@@ -4,14 +4,6 @@
     <input type="text" name="id" id="graphItemInput" class="itemSearch" placeholder="Start typing an item name...">
     <!-- <input type="submit" value="Graph Item" id="graphItemSubmit"> -->
     </form>
-    <select class="chzn-select hidden" id="timeRangeSelect">
-	    <option value="14">14 Days</option>
-	    <option value="30" selected>30 Days</option>
-	    <option value="60">60 Days</option>
-	    <option value="90">90 Days</option>
-	    <option value="120">120 Days</option>
-	    <option value="180">180 Days</option>
-    </select>
     <button id="saveGraphButton" class="hidden">Save</button>
 </div>
 <div id="graph"></div>
